0f646adf82076625c7e2ad192cc37f60defeb7aa
Haydar Oleyat <online@oleyat.no>
2026-04-24 12:58:43 +0200

    |a

--- app.py
+++ app.py
@@ -2,7 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-
+@app.route('/api/Students', methods=['GET'])
+def get_students():
+    return "students"
 
 if __name__ == "__main__":
     app.run(debug=True)
