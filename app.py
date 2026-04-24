826a1f936a5ab6a3f3b41666bd8436bfe76dee15
Elias <elias.n.bris05@gmail.com>
2026-04-24 13:28:58 +0200

    endringer

--- app.py
+++ app.py
@@ -8,5 +8,15 @@ app = Flask(__name__)
 def get_students():
     return getAllStudents()
 
+@app.route("/calculate-grades", methods=["POST"])
+def calculate_grades():
+
+    # Beregn og oppdater karakterer
+    for student in students:
+        student["grade"] = get_grade(student["marks"])
+
+    # Returner alle studenter med karakterer
+    return jsonify(students)
+
 if __name__ == "__main__":
     app.run(port=5000, debug=True)
